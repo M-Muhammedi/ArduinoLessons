8b4d41c2d07e91444129144b32aa9cb661db284e
Mustafa Muhammedi <146854084+M-Muhammedi@users.noreply.github.com>
2024-03-26 10:41:26 +0300

    Add files via upload

---
+++ ExampleVeriTipi/ExampleVeriTipi.ino
@@ -0,0 +1,43 @@
+  
+  int sayi = 10 ;
+  Serial. println ( "Değişken türü: int" );
+
+  long uzunSayi = 123456789 ;
+  Serial. println ( "Değişken türü: long" );
+
+  float ondalikliSayi = 3.14 ;
+  Serial. println ( "Değişken türü: float" );
+
+  double cokluOndalikliSayi = 3.14159265359 ;
+  Serial. println ( "Değişken türü: double" );
+
+  char karakter = 'A'; 
+  Serial. println ( "Değişken türü: char" );
+
+  boolean mantiksal = true ;
+  Serial. println ( "Değişken türü: boolean" );
+
+  byte baytDeger = 200 ;
+  Serial. println ( "Değişken türü: byte" );
+
+  unsigned int positiveInteger = 100 ;
+  Serial. println ( "Değişken türü: unsigned int" );
+
+  unsigned long bigPositiveInteger = 4294967295 ;
+  Serial. println ( "Değişken türü: unsigned long" );
+
+  unsigned char asciiCode = 'B';
+  Serial. println ( "Değişken türü: unsigned char" );
+
+  word sixteenBitInteger = 65535 ;
+  Serial. println ( "Değişken türü: word" );
+
+  short shortInteger = -32000 ;
+  Serial. println ( "Değişken türü: short" );
+
+  unsigned short positiveShortInteger = 60000 ;
+  Serial. println ( "Değişken türü: unsigned short" );
+
+  size_t memorySize = 1024 ;
+  Serial. println ( "Değişken türü: size_t" )
+
